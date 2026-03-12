bbdf5d26d35f2502c134e04e69de700c9a1f4f07
vrushankmhosamath <vrushankmhosamath@gmail.com>
2026-03-12 09:44:21 +0000

    Initial Commit to Git

---
+++ calculator.py
@@ -0,0 +1,23 @@
+print("Simple Calculator")
+num1=float(input("Enter 1st number"))
+num2=float(input("Enter 2nd number"))
+print("Choose operation")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+choice=input("enter choice")
+if choice == "1":
+	print("Result:",num1+num2)
+elif choice == "2":
+	print("Result:",num1-num2)
+elif choice == "3":
+	print("Result:" ,num1*num2)
+elif choice == "4":
+	if num2!=0:
+		print("Result:",num1/num2)
+	else:
+		print("Error : Division by zero")
+else:
+	print("Invalid choice")
+
