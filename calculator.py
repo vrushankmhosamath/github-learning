0ea02b46165785f92747dc5dc094ab1cadace658
vrushankmhosamath <vrushankmhosamath@gmail.com>
2026-03-13 10:14:28 +0000

    First hunk changed

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 print("Simple Calculator")
 num1=float(input("Enter 1st number"))
 num2=float(input("Enter 2nd number"))
+print("different hunk?")
 print("Choose operation")
 print("1.Add")
 print("2.Subtract")
